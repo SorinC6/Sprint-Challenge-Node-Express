29290fb4b62b9cc92a4d1d52e3f34c7880555232
Sorin Chis <kish.sorin@yahoo.com>
2019-03-22 11:49:09 +0200

    added endpoint request for update a project

--- api/routes/projectsRouter.js
+++ api/routes/projectsRouter.js
@@ -59,4 +59,25 @@ projectRoute.delete('/:id', async (req, res) => {
 	}
 });
 
+projectRoute.put('/:id', async (req, res) => {
+	const { id } = req.params;
+	const project = req.body;
+
+	if (project.name && project.description) {
+		try {
+			const result = await projectDb.update(id, project);
+			if (result) {
+				res.status(200).json(result);
+			} else {
+				res.status(404).json({ message: 'The project with specified id does not exists' });
+			}
+		} catch (error) {
+			res.status(500).json({ error: 'the project information coulld not be modified' });
+		}
+	} else {
+		res.status(400).json({ error: 'please provide a name and description for the project' });
+	}
+});
+
+
 module.exports = projectRoute;
