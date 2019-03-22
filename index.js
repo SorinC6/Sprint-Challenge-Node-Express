a2492003b44cdca289f27365bce764c2e0e71387
Sorin Chis <kish.sorin@yahoo.com>
2019-03-22 10:28:00 +0200

    added env and making the PORT dynamic

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
+require('dotenv').config();
 
 const server = require('./api/server');
 
-server.listen(4000, () => {
-	console.log(` \n*** Server in Running on http://localhost:4000 ***\n`);
+const PORT=process.env.PORT || 8000
+
+server.listen(PORT, () => {
+	console.log(` \n*** Server in Running on http://localhost:${PORT} ***\n`);
 });
