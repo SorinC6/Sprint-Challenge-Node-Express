55093346b3f53277ce66ea9605e51a4f48de9a4a
Sorin Chis <kish.sorin@yahoo.com>
2019-03-22 10:20:49 +0200

    added express server config, install nodemon and added script server and start in package.json

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
-// code away!
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+	res.send('<h1>Hello from the Server</h1>');
+});
+
+server.listen(4000, () => {
+	console.log(` \n*** Server in Running on http://localhost:4000 ***\n`);
+});
