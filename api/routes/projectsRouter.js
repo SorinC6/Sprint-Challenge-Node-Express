93c479569b4c7fb420d2e3deb123e40bab1e6064
Sorin Chis <kish.sorin@yahoo.com>
2019-03-22 11:22:53 +0200

    added endpoint for getting a project by id

--- api/routes/projectsRouter.js
+++ api/routes/projectsRouter.js
@@ -15,6 +15,18 @@ projectRoute.get('/', (req, res) => {
 		});
 });
 
-
+projectRoute.get('/:id', async (req, res) => {
+	const { id } = req.params;
+	try {
+		const project = await projectDb.get(id);
+		project
+			? res.status(200).json(project)
+			: res.status(404).json({
+					message: 'the project with specified id does not exists'
+				});
+	} catch (error) {
+		res.status(500).json({ error: 'The project information could not be retrived' });
+	}
+});
 
 module.exports = projectRoute;
